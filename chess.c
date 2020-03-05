3b3f72ddcc693dfaa3d58d685eb8ca816dd64216
Gil Lightnight <kosykh.igor11@gmail.com>
2020-03-05 08:43:50 +0700

    Created .c file with chess board

---
+++ chess.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char board[9][9]={{"XABCDEFGH"},
+		{"8rnbqkbnr"},
+		{"7pppppppp"},
+		{"6        "},
+		{"5        "},
+		{"4        "},
+		{"3        "},
+		{"2PPPPPPPP"},
+		{"1RNBQKBNR"}};
+
+	for (int i=0;i<9;i++)
+	{
+		for (int j=0;j<9;j++)
+		{
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
